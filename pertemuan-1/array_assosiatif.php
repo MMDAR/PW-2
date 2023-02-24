5336ceab3c84e07cda190684fa10aae330152100
MMDAR <mmmamir420@gmail.com>
2023-02-24 15:55:08 +0700

    praktikum web 2 sesi-1

---
+++ pertemuan-1/array_assosiatif.php
@@ -0,0 +1,10 @@
+<?php
+// array asosiatif
+$student = [
+    'name' => 'Muhammad Amir',
+    'nim' => 11234567,
+    'major' => 'informatika'
+];
+
+//mengkases array asosiatif
+echo "jurusan saya adalah  $student[major]";
